e653f81d8e2e36a878c7ee988e83c7657796d626
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-15 19:25:52 +0200

    add starting files

---
+++ app.js
@@ -0,0 +1,13 @@
+function Book (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        const readStatus = this.read ? "read" : "not read yet";
+        return (`${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`);
+    }
+};
+
+let harryPotter = new Book("Harry Potter","JK Rowling", 999, false);
+console.log(harryPotter.info());
