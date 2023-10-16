2471cb163c8624395eaba4906bc500bbcd2bbd53
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-16 23:39:23 +0200

    add basic modal and form

--- app.js
+++ app.js
@@ -80,4 +80,14 @@ function createBookCardDiv(title, author, pages, read) {
     topContent.appendChild(newBookCardDiv);
 };
 
-displayBooks()
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+
+openButton.addEventListener("click", () => {
+    modal.showModal();
+})
+
+closeButton.addEventListener("click", () => {
+    modal.close();
+})
