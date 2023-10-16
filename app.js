24ab116737b78485140459dcfb245dec6855165e
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-16 19:39:32 +0200

    add book cards and styles

--- app.js
+++ app.js
@@ -25,15 +25,37 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
 }
 
+//Write a function that loops through the array and 
+// displays each book on the page. 
+// You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
+
 // testing
-bookOne = new Book("book one", "author one", 1, true);
-bookTwo = new Book("book two", "author two", 2, false);
-bookThree = new Book("book three", "author three", 3, true);
+let bookOne = new Book("book one", "author one", 1, true);
+let bookTwo = new Book("book two", "author two", 2, false);
+let bookThree = new Book("book three", "author three", 3, true);
 
 myLibrary.push(bookOne);
 myLibrary.push(bookTwo);
 myLibrary.push(bookThree);
 
-function displayBook() {
-    
+let bookCard = document.querySelector("book-card");
+let bookTitle = document.querySelector("book__title");
+let bookAuthor = document.querySelector("book__author");
+let bookPages = document.querySelector("book__pages");
+let bookRead = document.querySelector("book__read");
+
+function displayBooks() {
+    for (const book of myLibrary) {
+        console.log(book.author);
+        console.log(book.info());
+    }
+}
+
+displayBooks();
+
+function createBookCard () {
+    const newBookCard = document.createElement('div');
+    newElement.textContent = 'This element appears on the screen';
+    document.body.appendChild(newElement);
+
 }
