78d27682fd21d034bfef0f2570e58d151b52b56e
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-17 22:52:24 +0200

    fix form submit. Book cards now show on screen

--- app.js
+++ app.js
@@ -12,38 +12,43 @@ function Book (title, author, pages, read) {
 
 // Library Array to hold Book Objects
 const myLibrary = [];
+const myLibraryAlreadyOnScreen = [];
+
+
 
 // Takes user input & adds Book Object to Library Array
-function addBookToLibrary() {
-    let titleInput = prompt("Book title: ");
-    let authorInput = prompt("Author: ");
-    let pagesInput = prompt("Nr of Pages: ");
-    let hasReadInput = prompt("Have you read the book? (y/n): ");
-    let hasRead = hasReadInput.toLowerCase() === "y" ? true : false;
-
-    newBook = new Book(titleInput, authorInput, pagesInput, hasRead);
+function addBookToLibrary(title, author, pages, read) {
+    newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-let bookshelf = document.querySelector(".bookshelf__content");
-let bookCard = document.querySelector(".book-card");
-let bookTitle = document.querySelector(".book__title");
-let bookAuthor = document.querySelector(".book__author");
-let bookPages = document.querySelector(".book__pages");
-let bookRead = document.querySelector(".book__read");
+// Bookshelf 
+const bookshelf = document.querySelector(".bookshelf__content");
+const bookCard = document.querySelector(".book-card");
+const bookTitle = document.querySelector(".book__title");
+const bookAuthor = document.querySelector(".book__author");
+const bookPages = document.querySelector(".book__pages");
+const bookRead = document.querySelector(".book__read");
+
+// Modal
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+const form = document.getElementById("form");
 
 // Displays all books currently stored in myLibrary array
 function displayBooks() {
-    for (const book of myLibrary) {
+    bookshelf.innerHTML = '';
+    for (const book of myLibrary){
         let currTitle = book["title"];
         let currAuthor = book["author"];
         let currPages = book["pages"];
         let currRead = book["read"];
-        createBookCardDiv(currTitle, currAuthor, currPages, currRead)
+        createBookCardDiv(currTitle, currAuthor, currPages, currRead);
     }
-}
+};
 
-// Creates a .book-card div inside .top-content div
+// Creates a .book-card div inside .bookshelf__content div
 function createBookCardDiv(title, author, pages, read) {
     const newBookCardDiv = document.createElement('div');
     newBookCardDiv.classList.add('book-card');
@@ -90,15 +95,25 @@ function createBookCardDiv(title, author, pages, read) {
     bookshelf.appendChild(newBookCardDiv);
 };
 
-const openButton = document.querySelector("[data-open-modal]");
-const closeButton = document.querySelector("[data-close-modal]");
-const modal = document.querySelector("[data-modal]");
-
+// Event Listeners
 openButton.addEventListener("click", () => {
     modal.showModal();
-})
+});
 
 closeButton.addEventListener("click", () => {
     modal.close();
-})
+});
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const titleInput = document.getElementById("book-title").value;
+    const authorInput = document.getElementById("book-author").value;
+    const pagesInput = document.getElementById("book-pages").value;
+    const readInputValue = document.querySelector(".book-read").value;
+    const readInput = readInputValue === "Yes" ? true : false;
+
+    addBookToLibrary(titleInput, authorInput, pagesInput, readInput);
+    form.reset();
+    modal.close();
+    displayBooks();
+});
